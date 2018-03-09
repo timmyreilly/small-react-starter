4386e3e504d1d4a30d22b3b5e8132b4116894d58
Tim Reilly <tireilly@microsoft.com>
2018-03-08 21:56:45 -0800

    initial commit

---
+++ src/js/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+// class Header extends React.Component {
+//     render() {
+//         return (
+//             <h1> Wazzup </h1>
+//         )
+//     }
+// }
+
+class Header extends React.Component {
+  constructor() {
+    super(); 
+    this.state = {
+      userName: "Tim"
+    }
+  }
+  render() {
+    return (
+      <h1>Hello, {this.state.userName}</h1>
+    )
+  }
+}
+
+ReactDOM.render(
+  <Header />,
+  document.getElementById('root')
+);
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const webpack = require('webpack');
+const path = require('path');
+
+/*
+ * We've enabled UglifyJSPlugin for you! This minifies your app
+ * in order to load faster and run less javascript.
+ *
+ * https://github.com/webpack-contrib/uglifyjs-webpack-plugin
+ *
+ */
+
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+	entry: "./src/js/index.js",
+	mode: 'development', 
+
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist')
+	},
+
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader',
+
+				options: {
+					presets: ['es2015', 'react']
+				}
+			},
+			{
+				test: /\.css$/,
+
+				use: [
+					{
+						loader: 'style-loader',
+
+						options: {
+							sourceMap: true
+						}
+					},
+					{
+						loader: 'css-loader'
+					}
+				]
+			}
+		]
+	},
+
+	plugins: [new UglifyJSPlugin()]
+};
